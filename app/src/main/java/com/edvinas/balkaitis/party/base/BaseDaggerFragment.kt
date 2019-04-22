a16e24cdf0697463fe5685ec471df0f5f68757f2
edvbal <edvinas.balkaitis@hermes-europe.co.uk>
2019-04-22 17:36:38 +0100

    Create BaseDaggerFragment#onResult

--- app/src/main/java/com/edvinas/balkaitis/party/base/BaseDaggerFragment.kt
+++ app/src/main/java/com/edvinas/balkaitis/party/base/BaseDaggerFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.edvinas.balkaitis.party.utils.mvvm.ViewModelFactory
@@ -23,6 +25,10 @@ abstract class BaseDaggerFragment : DaggerFragment() {
 
     protected fun <T : ViewModel> getViewModel(clazz: KClass<T>): T {
         return ViewModelProviders.of(this, viewModelFactory)[clazz.java]
+    }
+
+    protected fun <T> LiveData<T>.onResult(action: (T) -> Unit) {
+        observe(this@BaseDaggerFragment, Observer { data -> data?.let(action) })
     }
 
     @LayoutRes
